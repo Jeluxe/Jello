8a45c1472ea3a6f002140aeb91e4fd3315a5673a
Jeluxe <metalshay54@gmail.com>
2024-04-14 21:09:40 +0300

    Redesigned Project list.

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,14 +1,14 @@
-import Card from "../components/Card/Card"
+import Item from "../components/Item/Item"
 import "./Projects.css"
 
 const Projects = () => {
   return (
     <div className="main-container">
       <div className="projects-container">
-        <span style={{ width: "100%", paddingLeft: "8%" }}>My Projects</span>
+        <span className="container-title">My Projects</span>
         <div className="projects-wrapper">
-          {Array(15).fill(0).map((_, idx) => (
-            <Card key={idx} id={`foos${idx}`} title={`goose${idx}`} />
+          {Array(5).fill(0).map((_, idx) => (
+            <Item key={idx} id={`foos${idx}`} title={`goose${idx}`} />
           ))}
         </div>
       </div>
