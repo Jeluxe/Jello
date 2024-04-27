08a22fab49f0368cb0bf01f38c251617108dda96
Jeluxe <metalshay54@gmail.com>
2024-04-28 00:44:22 +0300

    style: Restyled Projects page to displayed properly.

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,12 +1,16 @@
-import { Item } from "../components"
+import { PlusIcon } from "../assets/icons"
+import { Button, Item } from "../components"
 import "./Projects.css"
 
 const Projects = () => {
   return (
     <div className="main-container">
       <div className="projects-container">
+        <div className="projects-header">
+          <span className="projects-header-title">My Projects</span>
+          <Button title={<PlusIcon />} size={40} style={{ padding: 0 }} onClick={() => console.log("first")} />
+        </div>
         <div className="projects-wrapper">
-          <span className="projects-header">My Projects</span>
           {Array(5).fill(0).map((_, idx) => (
             <Item key={idx} id={`foos${idx}`} title={`goose${idx}`} />
           ))}
