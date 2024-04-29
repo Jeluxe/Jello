cdb4c9e17ac3d2dd299e2e1625bc79edc2412a9e
Jeluxe <metalshay54@gmail.com>
2024-04-30 02:06:04 +0300

    chore: Reduced code in Dots component.

--- src/components/Dots/Dots.tsx
+++ src/components/Dots/Dots.tsx
@@ -4,9 +4,7 @@ import "./Dots.css"
 const Dots = ({ vertical = false, onClick, color, size, className = "" }: { vertical?: boolean, onClick?: any, color?: string, size?: number, className?: string[] | string }) => {
   return (
     <div className={separateClasses("dots", className)} style={{ flexDirection: vertical ? "column" : "row" }} onClick={onClick}>
-      <div style={{ background: color, height: size, width: size }} />
-      <div style={{ background: color, height: size, width: size }} />
-      <div style={{ background: color, height: size, width: size }} />
+      {Array(3).fill(0).map(() => <div style={{ background: color, height: size, width: size }} />)}
     </div>
   )
 }
