6949f0d7030e90fa0bcd4c21f2fe9b104d18a820
Jeluxe <metalshay54@gmail.com>
2024-05-02 19:09:28 +0300

    fix: Added index to divs in dots.
    so react wont complain.

--- src/components/Dots/Dots.tsx
+++ src/components/Dots/Dots.tsx
@@ -4,7 +4,7 @@ import "./Dots.css"
 const Dots = ({ vertical = false, onClick, color, size, className = "" }: { vertical?: boolean, onClick?: any, color?: string, size?: number, className?: string[] | string }) => {
   return (
     <div className={separateClasses("dots", className)} style={{ flexDirection: vertical ? "column" : "row" }} onClick={onClick}>
-      {Array(3).fill(0).map(() => <div style={{ background: color, height: size, width: size }} />)}
+      {Array(3).fill(0).map((_, idx) => <div key={idx} style={{ background: color, height: size, width: size }} />)}
     </div>
   )
 }
