109a1b240bf99c325d1d4fbb447407ae76ad907b
Jeluxe <metalshay54@gmail.com>
2024-05-05 20:03:10 +0300

    refactor: Refactored Button component to be more flexable.

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -3,14 +3,16 @@ import "./Button.css"
 
 type Props = {
   className?: string | string[],
-  size?: number,
+  size?: number | string,
+  width?: number | string,
+  height?: number | string,
   style?: any,
-  title: string,
+  title: string | React.ReactNode,
   onClick: (e: any) => void,
 }
-const Button = ({ className, size, style, title, onClick }: Props) => {
+const Button = ({ className, width, height, size, style, title, onClick }: Props) => {
   return (
-    <button className={separateClasses("button", className)} style={{ width: size, height: size, ...style }} onClick={onClick}>{title}</button>
+    <button className={separateClasses("button", className)} style={{ width: size || width, height: size || height, ...style }} onClick={onClick}>{title}</button>
   )
 }
 
