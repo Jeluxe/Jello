31f88ae72c0399c78e5b519844f3ca2956882e36
Jeluxe <metalshay54@gmail.com>
2024-05-05 20:04:04 +0300

    refactor: Refactored Divider.
    Moved all styles to the css file,
    and added type as class.

--- src/components/Divider/Divider.tsx
+++ src/components/Divider/Divider.tsx
@@ -1,29 +1,9 @@
+import { separateClasses } from "../../helpers";
 import "./Divider.css";
 
 const Divider = ({ type = "h", color }: { type?: string, color?: string }) => {
-  const verticalStyle = {
-    minHeight: "100%",
-    width: "1px",
-    alignItems: "center"
-  }
-
-  const horizonalStyle = {
-    minWidth: "100%",
-    height: "1px",
-    justifyContent: "center"
-  }
-
-  const findStyle = (type: string) => {
-    if (type === "h") {
-      return horizonalStyle
-    }
-    if (type === "v") {
-      return verticalStyle
-    }
-  }
-
   return (
-    <div className="divider" style={{ ...findStyle(type), background: color }}></div>
+    <div className={separateClasses("divider", type)} style={{ background: color }}></div>
   )
 }
 
