7118606ec49465e85f44ccef98cfa5f391d7064e
Jeluxe <metalshay54@gmail.com>
2024-05-13 01:57:41 +0300

    refactor: Refactored Projects page.
    Now projects page will display all of your projects,
    and will hold the new project form.

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,21 +1,52 @@
-import { PlusIcon } from "../assets/icons"
-import { Button, Item } from "../components"
+import { useState } from "react"
+import { Button, Item, Modal, NewProjectForm } from "../components"
+
+import { BackIcon, PlusIcon } from "../assets/icons"
+import { STATIC_DATA } from "../assets/global"
+
+import { ThemeProps } from "../types/global"
 import "./Projects.css"
+import { ThemeProviderData, ThemeProviderOperations, useThemeProvider } from "../context/ThemeContext"
+
+interface ProjectCardProps {
+  name: string,
+  theme: ThemeProps
+}
 
 const Projects = () => {
+  const { modalType, isModalOpen, setIsModalOpen }: Pick<ThemeProviderData, "modalType" | "isModalOpen"> & Pick<ThemeProviderOperations, "setIsModalOpen"> = useThemeProvider();
+  const [projectList, setProjectList] = useState<ProjectCardProps[]>(STATIC_DATA)
+  const [isNewProjectFormOpen, setIsNewProjectFormOpen] = useState<boolean>(false);
+
   return (
     <div className="main-container">
       <div className="projects-container">
         <div className="projects-header">
-          <span className="projects-header-title">My Projects</span>
-          <Button title={<PlusIcon />} size={40} style={{ padding: 0 }} onClick={() => console.log("first")} />
+          <span className="projects-header-title">
+            {isNewProjectFormOpen ? "New Project Form" : "My Projects"}
+          </span>
+          <Button
+            title={isNewProjectFormOpen ? <BackIcon /> : <PlusIcon />}
+            size={40}
+            style={{ padding: 0 }}
+            onClick={() => setIsNewProjectFormOpen(!isNewProjectFormOpen)}
+          />
         </div>
-        <div className="projects-wrapper">
-          {Array(5).fill(0).map((_, idx) => (
-            <Item key={idx} id={`foos${idx}`} title={`goose${idx}`} />
-          ))}
-        </div>
+        {isNewProjectFormOpen ?
+          <NewProjectForm
+            projectList={projectList}
+            setProjectList={setProjectList}
+            setIsNewProjectFormOpen={setIsNewProjectFormOpen}
+            setIsModalOpen={setIsModalOpen}
+          /> :
+          <div className="projects-wrapper">
+            {projectList.map((project, idx) => (
+              <Item key={idx} id={`foos${idx}`} title={`${project.name}`} theme={project.theme} />
+            ))}
+          </div>
+        }
       </div>
+      <Modal modalType={modalType} isModalOpen={isModalOpen} setIsModalOpen={setIsModalOpen} />
     </div>
   )
 }
