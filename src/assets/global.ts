306a2ba5784365aade07a7f604af18f9d4975ae3
Jeluxe <metalshay54@gmail.com>
2024-05-13 01:36:17 +0300

    chore: Added static data for now.

--- src/assets/global.ts
+++ src/assets/global.ts
@@ -1,8 +1,10 @@
 import { AcceptableImagesExtensions, ContainerMapProps, ThemeProps } from "../types/global";
+import monica from "./monic.jpg"
 
 export const ACCEPTABLE_IMAGE_FILE_EXTENSIONS: AcceptableImagesExtensions = [".jpg", ".jpeg", ".png"];
 
 export const PREDEFINED_THEMES: ThemeProps[] = [
+  { name: "monica", background: monica, isImage: true },
   { name: "blue", background: "blue" },
   { name: "yellow", background: "yellow" },
   { name: "green", background: "green" },
@@ -24,3 +26,10 @@ export const PROJECT_DATA: ContainerMapProps = {
   Completed: [{ id: "7", title: "Dofus", content: "ffoosoef32q", tags: ["info", "inspire"], participants: [{ id: "432t7gyf7wsef", username: "froooste", avatar: null }, { id: "432t7g7345wsef", username: "groooste11", avatar: null }, { id: "432t7g7345wsef1", username: "aroooste11", avatar: null }] }, { id: "8", title: "Jofus", content: "ffoosofsdf3", tags: [], participants: [] }, { id: "9", title: "Aofus", content: "fdfosadoso", tags: [], participants: [] }],
   Dropped: []
 }
+
+interface ProjectCardProps {
+  name: string,
+  theme: ThemeProps
+}
+
+export const STATIC_DATA: ProjectCardProps[] = Array(5).fill({ name: "goosse" }).map((item, idx) => ({ name: `${item.name}${idx}`, theme: Math.floor(Math.random() * 10) < 5 ? { name: "foseol", background: monica, isImage: true } : { name: "foseol", background: "black" } }))
