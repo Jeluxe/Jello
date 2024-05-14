9994e928d8165474c6e68ec9880d6fd0abeaf29d
Jeluxe <metalshay54@gmail.com>
2024-05-15 00:04:47 +0300

    feature: Created basic container modal.
    where user can for now change name, or delete the container.

--- src/components/Modal/ContainerModal/ContainerModal.tsx
+++ src/components/Modal/ContainerModal/ContainerModal.tsx
@@ -1,8 +1,58 @@
-import React from 'react'
+import { useState } from 'react'
+import Input from '../../Input/Input'
+import Button from '../../Button/Button'
+import { ProjectProviderOperations, useProjectProvider } from '../../../context/ProjectContext';
 
 const ContainerModal = ({ modalData }: { modalData: any }) => {
+  const { updateContainer, removeContainer, setModalData, setIsModalOpen }: ProjectProviderOperations = useProjectProvider()
+  const [containerName, setContainerName] = useState<string>(modalData.name)
+  const [newChanges, setNewChanges] = useState<boolean>(false)
+
+  const changeName = (value: string) => {
+    setNewChanges(false);
+    if (containerName !== value) {
+      setNewChanges(true);
+    }
+    setContainerName(value)
+  }
+
+  const saveChanges = () => {
+    if (newChanges) {
+      updateContainer(modalData.name, containerName)
+    }
+    closeModal()
+  }
+
+  const cancelChanges = () => {
+    if (!newChanges) {
+      closeModal()
+    } else if (confirm("do you wish to discard the changes you have made?")) {
+      closeModal()
+    }
+  }
+
+  const deleteContainer = () => {
+    removeContainer(modalData.name)
+    closeModal()
+  }
+
+  const closeModal = () => {
+    setModalData(null);
+    setIsModalOpen(false)
+  }
+
   return (
-    <div>ContainerModal</div>
+    <div className='modal-wrapper'>
+      <div>
+        <span>name:</span> <Input value={containerName ?? modalData.name} onChange={(e) => changeName(e.target.value)} />
+
+        <div className='container-modal-actions'>
+          <Button title={"save"} width={80} padding={false} height={40} onClick={saveChanges} />
+          <Button title={"cancel"} color='orange' padding={false} width={80} height={40} onClick={cancelChanges} />
+          <Button title={"delete"} color='salmon' padding={false} width={80} height={40} onClick={deleteContainer} />
+        </div>
+      </div>
+    </div>
   )
 }
 
