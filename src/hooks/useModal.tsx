58062ec21003143f36dec08c1d0ccd40148d39ac
Jeluxe <metalshay54@gmail.com>
2024-05-14 16:31:46 +0300

    chore: Added useState for modalType.

--- src/hooks/useModal.tsx
+++ src/hooks/useModal.tsx
@@ -2,11 +2,11 @@ import { useState } from "react";
 import { ModalDataTypes, ModalTypes } from "../types/global";
 
 const useModal = (type: ModalTypes) => {
-  const modalType: ModalTypes = type;
+  const [modalType, setModalType] = useState<ModalTypes>(type);
   const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
   const [modalData, setModalData] = useState<ModalDataTypes | null>(null)
 
-  return { modalType, isModalOpen, setIsModalOpen, modalData, setModalData }
+  return { modalType, isModalOpen, modalData, setIsModalOpen, setModalData, setModalType }
 }
 
 export default useModal
